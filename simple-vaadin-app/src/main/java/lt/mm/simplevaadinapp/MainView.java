db9696cf3d23082c7ff786529bce6aa8081bafd7
Martynas Mitrulevičius <martynas.mitrulevicius@spp.se>
2019-08-05 14:24:18 +0300

    code cleanup

--- simple-vaadin-app/src/main/java/lt/mm/simplevaadinapp/MainView.java
+++ simple-vaadin-app/src/main/java/lt/mm/simplevaadinapp/MainView.java
@@ -6,7 +6,6 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
-
 @Route
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
