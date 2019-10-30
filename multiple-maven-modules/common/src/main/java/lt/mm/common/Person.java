04cf1e75513f96573fb61d8b82ca809444c1127f
Martynas <44275503+martynas-mit@users.noreply.github.com>
2019-10-30 21:35:16 +0200

    maven multi module

---
+++ multiple-maven-modules/common/src/main/java/lt/mm/common/Person.java
@@ -0,0 +1,14 @@
+package lt.mm.common;
+
+public class Person {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
